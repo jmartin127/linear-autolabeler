18f1c511af84249b63b251e685ea10878354baa3
Jeff Martin <jeff.martin@getweave.com>
2020-06-19 14:22:32 -0600

    Query for teams is working

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jmartin127/linear-autolabeler
+
+go 1.14
+
+require (
+	github.com/graphql-go/graphql v0.7.9
+	github.com/machinebox/graphql v0.2.2
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/machinebox/graphql"
+)
+
+const (
+	authToken  = "<auth-token-here>"
+	teamsQuery = `
+	{
+		teams {
+		  nodes {
+			id
+			name
+		  }
+		}
+	  }
+	`
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	graphqlClient := graphql.NewClient("https://api.linear.app/graphql")
+	graphqlRequest := graphql.NewRequest(teamsQuery)
+
+	headers := make(map[string][]string)
+	headers["Authorization"] = []string{authToken}
+	graphqlRequest.Header = headers
+
+	var graphqlResponse interface{}
+	if err := graphqlClient.Run(context.Background(), graphqlRequest, &graphqlResponse); err != nil {
+		panic(err)
+	}
+	fmt.Println(graphqlResponse)
 }
