b7b395e1324011517c470f3a89e5e094f38cb3b5
Jeff Martin <jeff.martin@getweave.com>
2020-06-27 14:31:52 -0600

    Adding/removing labels from tickets is working

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/machinebox/graphql"
@@ -71,17 +72,29 @@ const (
 
 	issueCommentsQuery = `{
 		issue(id: "%s") {
-		  id
-		  title
-		  description
-		  comments {
-			nodes {
-			  createdAt
-			  body
+			id
+			title
+			description
+			comments {
+				nodes {
+					createdAt
+					body
+				}
 			}
-		  }
 		}
-	  }`
+	}`
+
+	issueLabelsQuery = `{
+		issue(id: "%s") {
+			id
+			labels {
+				nodes {
+					id
+					name
+				}
+			}
+		}
+	}`
 
 	workflowStatesQuery = `{
 		workflowStates {
@@ -91,10 +104,47 @@ const (
 		  }
 		}
 	  }`
+
+	addIssueLabelMutation = `mutation {
+		issueUpdate(
+		  id: "%s",
+		  input: {
+			labelIds: [%s]
+		  }
+		) {
+		  success
+		  issue {
+			id
+			title
+		  }
+		}
+	  }`
+
+	labelsQuery = `{
+		team(id: "%s") {
+			id
+			name
+		
+			labels {
+				nodes {
+					id
+					name
+				}
+			}
+		}
+	}`
 )
 
 type TeamIssuesResponse struct {
 	Team Team `json:"team"`
+}
+
+type TeamLabelsResponse struct {
+	TeamLabels TeamLabels `json:"team"`
+}
+
+type TeamLabels struct {
+	IssueLabels IssueLabels `json:"labels"`
 }
 
 type Team struct {
@@ -135,6 +185,7 @@ type IssueNode struct {
 	TeamName      TeamName      `json:"team"`
 	IssueHistory  IssueHistory  `json:"history"`
 	IssueComments IssueComments `json:"comments"`
+	IssueLabels   IssueLabels   `json:"labels"`
 }
 
 type Assignee struct {
@@ -155,6 +206,10 @@ type IssueComments struct {
 	Nodes []IssueCommentNode `json:"nodes"`
 }
 
+type IssueLabels struct {
+	Nodes []IssueLabelNode `json:"nodes"`
+}
+
 type IssueHistoryNode struct {
 	CreatedAt time.Time     `json:"createdAt"`
 	FromState WorkflowState `json:"fromState"`
@@ -166,8 +221,21 @@ type IssueCommentNode struct {
 	Body      string    `json:"body"`
 }
 
+type IssueLabelNode struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 type WorkflowState struct {
 	Name string `json:"name"`
+}
+
+type IssueUpdateResponse struct {
+	IssueUpdate IssueUpdate `json:"issueUpdate"`
+}
+
+type IssueUpdate struct {
+	Success bool `json:"success"`
 }
 
 type LinearClient struct {
@@ -184,15 +252,26 @@ func main() {
 		token: authToken,
 	}
 
+	// TODO load the Team ID, given the team name
+	teamID := "99dea3d2-59ff-4273-b8a1-379d36bb1678"
+
+	// find the "ExceedsSLA" label
+	exceedsSLALabelID, err := lc.findLabelIDWithName(teamID, "ExceedsSLA")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	pagination := "first:50"
 	var totalIssues int
 
+	// TODO the timezone should be configurable
 	loc, err := time.LoadLocation("America/Denver")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for true {
+		// TODO the Team ID should be configurable
 		query := fmt.Sprintf(issuesQuery, "99dea3d2-59ff-4273-b8a1-379d36bb1678", pagination)
 
 		var response TeamIssuesResponse
@@ -202,12 +281,20 @@ func main() {
 
 		for _, v := range response.Team.Issues.Edges {
 			exceeds, durationExceeding := lc.exceedsSLA(&v.IssueNode, loc)
+			ticketNumber := ticketNumber(&v.IssueNode)
 			if exceeds {
 				lastComment, err := lc.getLastTimeIssueWasCommentedOn(&v.IssueNode)
 				if err != nil {
 					log.Fatal(err)
 				}
-				fmt.Printf("Exceeds SLA: %+v, Ticket: %d, State: %s, TeamKey: %s, LastComment: %+v\n", durationExceeding, v.IssueNode.Number, v.IssueNode.State.Name, v.IssueNode.TeamName.Key, lastComment)
+				fmt.Printf("Exceeds SLA: %+v, Ticket: %s, State: %s, LastComment: %+v\n", durationExceeding, ticketNumber, v.IssueNode.State.Name, lastComment)
+				if err := lc.addLabelToTicket(ticketNumber, exceedsSLALabelID); err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				if err := lc.removeLabelFromTicket(ticketNumber, exceedsSLALabelID); err != nil {
+					log.Fatal(err)
+				}
 			}
 			totalIssues++
 		}
@@ -221,21 +308,127 @@ func main() {
 	}
 
 	fmt.Printf("Total issues: %d\n", totalIssues)
+}
 
-	// var response interface{}
-	// if err := exectueQuery(query, &response); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("INterface: %+v\n", response)
+func (lc *LinearClient) findLabelIDWithName(teamID string, labelName string) (string, error) {
+	labels, err := lc.getTeamLabels(teamID)
+	if err != nil {
+		return "", err
+	}
 
+	for _, l := range labels {
+		if l.Name == labelName {
+			return l.ID, nil
+		}
+	}
+
+	return "", fmt.Errorf("cannot find label with name %s", labelName)
+}
+
+func (lc *LinearClient) getTeamLabels(teamID string) ([]IssueLabelNode, error) {
+	query := fmt.Sprintf(labelsQuery, teamID)
+
+	var response TeamLabelsResponse
+	err := lc.exectueQuery(query, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.TeamLabels.IssueLabels.Nodes, nil
+}
+
+func (lc *LinearClient) addLabelToTicket(ticketNumber string, labelID string) error {
+	// get current set of labels
+	labels, err := lc.getLabels(ticketNumber)
+	if err != nil {
+		return err
+	}
+
+	// get the labelIDs from the labels
+	labelIDs := make([]string, 0)
+	for _, l := range labels {
+
+		// if this label already exists, do not add it again
+		if l.ID == labelID {
+			fmt.Printf("\tlabel already exists for ticket %s\n", ticketNumber)
+			return nil
+		}
+		labelIDs = append(labelIDs, l.ID)
+	}
+
+	// add the new label to the list
+	labelIDs = append(labelIDs, labelID)
+
+	// apply the labels
+	fmt.Printf("Adding label to ticket: %s\n", ticketNumber)
+	return lc.applyLabels(ticketNumber, labelIDs)
+}
+
+func (lc *LinearClient) removeLabelFromTicket(ticketNumber string, labelID string) error {
+	// get current set of labels
+	labels, err := lc.getLabels(ticketNumber)
+	if err != nil {
+		return err
+	}
+
+	// get the labelIDs from the labels
+	labelIDs := make([]string, 0)
+	var foundLabel bool
+	for _, l := range labels {
+		// do not add the label that we are removing to the list
+		if l.ID == labelID {
+			foundLabel = true
+		} else {
+			labelIDs = append(labelIDs, l.ID)
+		}
+	}
+
+	if foundLabel { // no need to apply the labels if it wasn't in the list
+		// apply the labels
+		fmt.Printf("Found label, removing from ticket %s\n", ticketNumber)
+		return lc.applyLabels(ticketNumber, labelIDs)
+	}
+
+	return nil
+}
+
+func (lc *LinearClient) applyLabels(ticketNumber string, labelIDs []string) error {
+	labelIDsString := strings.Join(labelIDs, `", "`)
+	labelIDsString = fmt.Sprintf(`"%s"`, labelIDsString) // need this format: "id1", "id2", "id3"
+	mutation := fmt.Sprintf(addIssueLabelMutation, ticketNumber, labelIDsString)
+
+	var response IssueUpdateResponse
+	err := lc.exectueQuery(mutation, &response)
+	if err != nil {
+		return err
+	}
+
+	if !response.IssueUpdate.Success {
+		return fmt.Errorf("Applying labels did not succeed for ticket %s", ticketNumber)
+	}
+
+	return nil
+}
+
+func (lc *LinearClient) getLabels(ticketNumber string) ([]IssueLabelNode, error) {
+	query := fmt.Sprintf(issueLabelsQuery, ticketNumber)
+
+	var response IssueResponse
+	err := lc.exectueQuery(query, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Issue.IssueLabels.Nodes, nil
 }
 
 func (lc *LinearClient) exectueQuery(query string, response interface{}) error {
-	graphqlClient := graphql.NewClient("https://api.linear.app/graphql")
+	graphqlClient := graphql.NewClient("https://api.linear.app/graphql") // TODO only do this once in the client itself
 	graphqlRequest := graphql.NewRequest(query)
 
 	headers := make(map[string][]string)
 	headers["Authorization"] = []string{lc.token}
+
 	graphqlRequest.Header = headers
 
 	if err := graphqlClient.Run(context.Background(), graphqlRequest, &response); err != nil {
@@ -292,8 +485,12 @@ func exceedsSLAInBusinessHoursForStart(refTime time.Time, loc *time.Location, sl
 	return false, time.Hour
 }
 
+func ticketNumber(issue *IssueNode) string {
+	return fmt.Sprintf("%s-%d", issue.TeamName.Key, issue.Number)
+}
+
 func (lc *LinearClient) getLastTimeIssueWasCommentedOn(issue *IssueNode) (time.Time, error) {
-	ticketNumber := fmt.Sprintf("%s-%d", issue.TeamName.Key, issue.Number)
+	ticketNumber := ticketNumber(issue)
 	comments, err := lc.getIssueComments(ticketNumber)
 	if err != nil {
 		return time.Time{}, err
